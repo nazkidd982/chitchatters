99075f94369922ca58d0c75744f3e7e8c05ae81e
N42K1dd <ahmadnazem2010@gmail.com>
2022-10-10 22:07:59 +0800

    [221010::Nazem] Updated to latest vue typescript

--- resources/js/components/ChatBoard.vue
+++ resources/js/components/ChatBoard.vue
@@ -1,97 +1,90 @@
 <template>
-	<div class="chat card">
-		<div class="scrollable card-body" ref="hasScrolledToBottom">
-	        <template v-for="message in messages">
-	            <div class="message message-receive" v-if="user.id != message.user.id">
-	                <p>
-	                    <strong class="primary-font">
-	                        {{ message.user.name }} :
-	                    </strong>
-	                    {{ message.message }}
-	                </p>
-	            </div>
-	            <div class="message message-send" v-else>
-	                <p>
-	                    <strong class="primary-font">
-	                        {{ message.user.name }} :
-	                    </strong>
-	                    {{ message.message }}
-	                </p>
-	            </div>
-	        </template>
-	    </div>
+    <div class="chat card">
+        <div ref="hasScrolledToBottom" class="scrollable card-body">
+            <template v-for="message in messages">
+                <div v-if="user.id !== message.user.id" class="message message-receive">
+                    <p>
+                        <strong class="primary-font">
+                            {{ message.user.name }} :
+                        </strong>
+                        {{ message.message }}
+                    </p>
+                </div>
+                <div v-else class="message message-send">
+                    <p>
+                        <strong class="primary-font">
+                            {{ message.user.name }} :
+                        </strong>
+                        {{ message.message }}
+                    </p>
+                </div>
+            </template>
+        </div>
 
-	    <div class="chat-form input-group">
-	        <input id="btn-input" type="text" name="message" class="form-control input-sm message-" placeholder="Type your message here..." v-model="newMessage" @keyup.enter="addMessage">
+        <div class="chat-form input-group">
+            <input id="btn-input" v-model="newMessage" class="form-control input-sm message-" name="message"
+                   placeholder="Type your message here..." type="text" @keyup.enter="addMessage">
 
-	        <span class="input-group-btn">
-	            <button class="btn btn-primary" id="btn-chat" @click="addMessage">
+            <span class="input-group-btn">
+	            <button id="btn-chat" class="btn btn-primary" @click="addMessage">
 	                Send
 	            </button>
 	        </span>
-	    </div>
+        </div>
 
-	</div>
+    </div>
 </template>
 
-<script>
-	import { reactive, inject,ref, onMounted,onUpdated } from 'vue';
-	import axios from 'axios';
-	export default{
-		props:['user'],
-	    setup(props){
-	    	let messages = ref([])
-	    	let newMessage = ref('')
-	    	let hasScrolledToBottom = ref('')
+<script setup>
+import {onMounted, onUpdated, ref} from 'vue';
+import axios from 'axios';
 
-	    	onMounted(() =>{
-	    		fetchMessages()
-	    	})
+const props = defineProps({
+    user: Object,
+});
 
-	    	onUpdated(() => {
-	    		scrollBottom()
-	    	})
+const messages = ref([]);
+const newMessage = ref('');
+const hasScrolledToBottom = ref('');
 
-	    	Echo.private('chat-channel')
-	          .listen('SendMessage', (e) => {
-	            messages.value.push({
-	              message: e.message.message,
-	              user: e.user
-	            });
-	        })
+onMounted(() => {
+    fetchMessages();
+});
 
-	    	const fetchMessages = async()=> {
-	            axios.get('/messages').then(response => {
-	                messages.value = response.data;
-	            });
-	        }
+onUpdated(() => {
+    scrollBottom();
+});
 
-	        const addMessage = async()=> {
-	        	let user_message = {
-                    user: props.user,
-                    message: newMessage.value
-                };
-	            messages.value.push(user_message);
-	            axios.post('/messages', user_message).then(response => {
-	              console.log(response.data);
-	            });
-                newMessage.value = ''
-	        }
+Echo.private('chat-channel')
+    .listen('SendMessage', (e) => {
+        messages.value.push({
+            message: e.message.message,
+            user: e.user
+        });
+    });
 
-	        const scrollBottom = () =>{
-	        	if(messages.value.length > 1){
-		        	let el = hasScrolledToBottom.value;
-	      			el.scrollTop = el.scrollHeight;
-	        	}
-	        }
+const fetchMessages = async () => {
+    axios.get('/messages').then(response => {
+        messages.value = response.data;
+    });
+};
 
-	        return {
-	        	messages,
-	        	newMessage,
-	        	addMessage,
-	        	fetchMessages,
-	        	hasScrolledToBottom
-	        }
-	    }
-	}
+const addMessage = async () => {
+    let user_message = {
+        user: props.user,
+        message: newMessage.value
+    };
+    messages.value.push(user_message);
+    axios.post('/messages', user_message).then(response => {
+        // console.log(response.data);
+    });
+    newMessage.value = ''
+};
+
+const scrollBottom = () => {
+    if (messages.value.length > 1) {
+        let el = hasScrolledToBottom.value;
+        el.scrollTop = el.scrollHeight;
+    }
+};
 </script>
